f8b1539b7470a8f5e3ba87da6e41713a1a79fa5a
Yamato Osada <yamatoosada86@gmail.com>
2023-11-26 18:52:57 +0900

    add test.bash

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nonaka',
+    maintainer_email='nonakashinnosuke@gmail.com',
+    description='TODO: Package description',
+    license='BSD-3-Clause ',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
